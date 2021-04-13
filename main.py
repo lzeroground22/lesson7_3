978ad26729e449f91221abac07bb9eeed247c549
Alex <User@Big-PC.les.local>
2021-04-13 22:15:58 +0300

    ready

---
+++ main.py
@@ -0,0 +1,22 @@
+documents = ['1.txt', '2.txt', '3.txt']
+
+
+def counter(txt):
+    line_list = list()
+    for file in txt:
+        with open(file, encoding='utf-8') as f:
+            lines = f.readlines()
+            line_dict = {'lines': len(lines), 'file': file}
+            line_list.append(line_dict)
+    low_high_list = sorted(line_list, key=lambda i: i['lines'])
+    return low_high_list
+
+
+with open('result.txt', 'w') as fw:
+    low_high_list = counter(documents)
+    for doc in low_high_list:
+        file_str = f'{doc["file"]}\n'
+        fw.write(file_str)
+        line_str = f'{doc["lines"]}\n'
+        fw.write(line_str)
+        fw.write('\n')
